a95c39dfd3686f066e201159127ef6f62135ea79
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2019-09-04 11:50:22 -0500

    Update util_tf_labels.py
    Add normalization to dataset creator

--- util_tf_labels.py
+++ util_tf_labels.py
@@ -62,11 +62,13 @@ def create_xy_labels(im_dir,label_dir,calib_dir):
     """    
     camera_space_labels = []
     image_space_labels = []
-    
+    depth_labels = []
     data = Track_Dataset(im_dir,label_dir,calib_dir)    
     for i in range(0, len(data.label_list)):
         
         data.load_track(i)
+        im,_ = next(data)
+        im_size = im.size[:2]
         labels = data.labels
         P = data.calib
         
@@ -90,33 +92,56 @@ def create_xy_labels(im_dir,label_dir,calib_dir):
                     tf_coords = tf_coords.reshape(16)
                     
                     #organize camera_space features
-                    image_space = get_image_space_features(tf_coords,P)
+                    image_space = get_image_space_features(tf_coords,P,im_size)
                     
                     # remove examples behind camera
                     if X > 0 and det_dict['truncation'] == 0:
                         camera_space_labels.append(camera_space)
                         image_space_labels.append(image_space)
+                        depth_labels.append(tf_depths)
+            
+            
             
     image_space_labels = np.asarray(image_space_labels)
     camera_space_labels = np.asarray(camera_space_labels)
-    return image_space_labels,camera_space_labels, tf_depths
+    depth_labels = np.asarray(depth_labels) /100 # to normalize
+     
+    return image_space_labels,depth_labels, camera_space_labels 
  
 
-def get_image_space_features(tf_coords,P):
+def get_image_space_features(tf_coords,P,im_size):
+    
+    im_size = np.asarray(im_size)[None,:] # add second axis
+    
     # flatten camera calibration matrix
     P_flat = P.reshape(12)
-    
+    c = tf_coords # shorten name for expressions below
+    dist = lambda x1,y1,x2,y2: np.sqrt((x1-x2)**2 + (y1-y2)**2)
     # get a bunch of ratios in terms of image_space
-    fhr =
-    fwr =
-    tlr = 
-    blr = 
-    rhr = 
-    rwr = 
+    fhr = dist(c[0],c[8],c[4],c[12])  / dist(c[1],c[9],c[5],c[13])
+    fwr = dist(c[0],c[8],c[1],c[9])   / dist(c[4],c[12],c[5],c[13])
+    tlr = dist(c[0],c[8],c[3],c[11])  / dist(c[1],c[9],c[2],c[10])
+    blr = dist(c[4],c[12],c[7],c[15]) / dist(c[5],c[13],c[6],c[14])
+    rhr = dist(c[3],c[11],c[7],c[15]) / dist(c[2],c[12],c[6],c[14])
+    rwr = dist(c[3],c[11],c[2],c[12]) / dist(c[7],c[15],c[6],c[14])
     
-    lw  = 
-    lh  = 
+    lw  = (dist(c[0],c[8],c[3],c[11]) + dist(c[1],c[9],c[2],c[10])) / \
+    (dist(c[3],c[11],c[2],c[12]) + dist(c[0],c[8],c[1],c[9]) )
     
+    lh  = (dist(c[0],c[8],c[3],c[11]) + dist(c[1],c[9],c[2],c[10])) / \
+    (dist(c[3],c[11],c[7],c[15]) + dist(c[2],c[12],c[6],c[14]))
+    
+    ratios = [fhr,fwr,tlr,blr,rhr,rwr,lw,lh]
+    
+    # normalize ratios by dividing by 10
+    ratios = [j/10 for j in ratios]
+    # normalize P by dividing by 1000
+    P = [k/1000 for k in P]
+    # normalize coords by dividing by image size
+    tf_coords = tf_coords.reshape(8,2) / im_size
+    tf_coords = tf_coords.reshape(16)
+    ret = np.array([i for i in tf_coords] + [j for j in ratios] + [k for k in P_flat])
+    return ret
     
     
 #def decode_frame_labels(label,P):
@@ -164,7 +189,7 @@ def create_datasets(im_dir,label_dir,calib_dir, val_ratio = 0.2, seed = 0):
     """
     """
     random.seed = 0
-    X,Y = create_xy_labels(im_dir,label_dir,calib_dir)
+    X,Y,camera_space = create_xy_labels(im_dir,label_dir,calib_dir)
     
     n_examples = len(X)
     
@@ -178,25 +203,11 @@ def create_datasets(im_dir,label_dir,calib_dir, val_ratio = 0.2, seed = 0):
     X_test = X[test_idx,:]
     Y_train = Y[train_idx,:]
     Y_test = Y[test_idx,:]  
-    
-    # TODOO - better normalization
-    # normalize by subtracting min value, dividing by range
-    if True:
-        X_min = np.min(X,0)
-        Y_min = np.min(Y,0)
-        X_avg = np.average(X,0)
-        Y_avg = np.average(Y,0)
-        X_max = np.max(X,0)
-        Y_max = np.max(Y,0)
-        X_train = (X_train -X_min[None,:])  /(X_avg)[None,:]
-        X_test =  (X_test  -X_min[None,:])  / (X_avg)[None,:]
-        Y_train = (Y_train -Y_min[None,:])  / (Y_avg)[None,:]
-        Y_test =  (Y_test   -Y_min[None,:]) / (Y_avg)[None,:]
         
     train_data = Dataset(X_train,Y_train)
     test_data = Dataset(X_test,Y_test)
     
-    return train_data,test_data
+    return train_data,test_data,camera_space
 
   
     
@@ -206,4 +217,4 @@ if __name__ == "__main__":
     train_im_dir =    "C:\\Users\\derek\\Desktop\\KITTI\\Tracking\\Tracks\\training\\image_02"  
     train_lab_dir =   "C:\\Users\\derek\\Desktop\\KITTI\\Tracking\\Labels\\training\\label_02"
     train_calib_dir = "C:\\Users\\derek\\Desktop\\KITTI\\Tracking\\data_tracking_calib(1)\\training\\calib"
-    X,Y = create_datasets(train_im_dir,train_lab_dir,train_calib_dir)   
+    train_data,test_data,camera_space = create_datasets(train_im_dir,train_lab_dir,train_calib_dir)   
